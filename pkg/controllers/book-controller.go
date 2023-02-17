0afef3183300571b1dcb4ac0eb4ded5187518554
Wesley Matos <wm4tos.777@gmail.com>
2023-02-17 14:11:35 -0300

    feat: create base controllers

---
+++ pkg/controllers/book-controller.go
@@ -0,0 +1,15 @@
+package controllers
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func GetBook(ctx *gin.Context) {}
+
+func GetBookById(ctx *gin.Context) {}
+
+func CreateBook(ctx *gin.Context) {}
+
+func DeleteBook(ctx *gin.Context) {}
+
+func UpdateBook(ctx *gin.Context) {}
