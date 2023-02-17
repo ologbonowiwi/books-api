cee32fd9a9a4e0ccfacb5e838445d199b8fd9d1f
Wesley Matos <wm4tos.777@gmail.com>
2023-02-17 14:15:42 -0300

    refactor: change name

--- pkg/routes/bookstore-routes.go
+++ pkg/routes/bookstore-routes.go
@@ -5,14 +5,14 @@ import (
 	"github.com/ologbonowiwi/bookstore-api/pkg/controllers"
 )
 
-func RegisterRoutes(engine *gin.Engine) {
-	engine.Use(func(c *gin.Context) {
+func RegisterRoutes(router *gin.Engine) {
+	router.Use(func(c *gin.Context) {
 		c.Writer.Header().Set("Content-Type", "application/json")
 		c.Next()
 	})
-	engine.GET("/books/", controllers.GetBook)
-	engine.POST("/books/", controllers.CreateBook)
-	engine.GET("/books/:id", controllers.GetBookById)
-	engine.PUT("/books/:id", controllers.UpdateBook)
-	engine.DELETE("/books/:id", controllers.DeleteBook)
+	router.GET("/books/", controllers.GetBook)
+	router.POST("/books/", controllers.CreateBook)
+	router.GET("/books/:id", controllers.GetBookById)
+	router.PUT("/books/:id", controllers.UpdateBook)
+	router.DELETE("/books/:id", controllers.DeleteBook)
 }
